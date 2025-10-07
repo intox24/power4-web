810532e8c13c308d9d338afda05817dffffaabcb
spjulien <julien.specte@ynov.com>
2025-10-07 17:25:01 +0200

    Ajout du formulaire J1 J2 difficulté et boutton commencer la partie

--- server.go
+++ server.go
@@ -14,8 +14,17 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	template.Execute(w, nil)
 }
 
+func Play(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./pages/play.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, nil)
+}
+
 func main() {
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/play", Play)
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.ListenAndServe(":8080", nil)
