76e5e9b372702fd7af5a61922e9b2e7c3cfcd037
spjulien <julien.specte@ynov.com>
2025-10-07 15:07:48 +0200

    création des différents fichier et du serveur en go

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.ListenAndServe(":8080", nil)
+}
